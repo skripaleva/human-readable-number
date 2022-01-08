5c10dbb9b3160e5044ad1d212f48875c6bb13894
Evgeniya Skripaleva <evgeniya_ignaten@mail.ru>
2022-01-08 23:34:03 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function toReadable (number) {
-  
-}
+  let numbers = ['zero', 'one', 'two', 'three', 'four' , 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve',
+      'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  let dozens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    if (number < 20) return numbers[number];
+    if (number < 100) return (number % 10 === 0) ? dozens[number/10] : dozens[Math.floor(number/10)] + ' ' + toReadable(number%10);
+    if (number < 1000) return (number % 100 === 0) ? numbers[number/100] + ' hundred': numbers[Math.floor(number/100)] + ' hundred ' + toReadable(number%100);
+};
